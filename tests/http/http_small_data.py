69a89b552a2aac571a25061a163bfc9041805d58
jakulej <j.kulej01@gmail.com>
2025-06-03 18:43:43 +0200

    created big data test

--- tests/http/http_small_data.py
+++ tests/http/http_small_data.py
@@ -3,9 +3,6 @@ import time
 import json
 import subprocess
 
-
-
-
 URL = 'http://127.0.0.1:5000'
 
 def simple_reqest(i,output_file,url):
@@ -46,26 +43,19 @@ def test_independent_call(i,output_file):
     with open(output_file, "w") as f:
         f.write("request_number,time_ms\n")
     for n in range(i):
-        result = subprocess.run(["python3", "tests/http/single_request.py", str(n)], capture_output=True, text=True)
+        result = subprocess.run(["python3", "tests/http/single_request_small.py", str(n)], capture_output=True, text=True)
 
-def test_big_file(i, output_file,url):
-    with open('data/small_data.json') as file:
-        small_data = json.load(file)
+def test_big_file(i, output_file,url,big_data):
 
     with open(output_file, "w") as f:
         f.write("request_number,time_ms\n")
 
-    client = httpx.Client()
-
     for n in range(i):
-        start = time.perf_counter()
-        response = client.post(url,json=small_data)
-        end = time.perf_counter()
+        for n in range(i):
+            result = subprocess.run(["python3", "tests/http/single_request_small.py", str(n)], capture_output=True, text=True)
 
-        with open(output_file, "a") as f:
-            f.write(f"{n},{(end-start)*1000}\n")
-    client.close()
 
-test_independent_call(100,"output/http_simple_small_independent.csv")
-simple_reqest(100,"output/http_simple_small.csv",URL)
-client_request(100,"output/http_client_small.csv",URL)
+#test_independent_call(100,"output/http_simple_small_independent.csv")
+#simple_reqest(100,"output/http_simple_small.csv",URL)
+#client_request(100,"output/http_client_small.csv",URL)
+test_big_file(100,"output/http_client_big.csv",URL+"/file","data/big_file")
---
+++ tests/http/single_request_big.py
@@ -0,0 +1,18 @@
+import httpx
+import time
+import json
+import sys 
+
+OUTPUT_FILE = "output/http_client_big.csv"
+URL = 'http://127.0.0.1:5000/file'
+
+with open('data/big_file') as file:
+    small_data = json.load(file)
+
+start = time.perf_counter()
+response = httpx.post(URL,files={"file":big_data})
+end = time.perf_counter()
+
+
+with open(OUTPUT_FILE, "a") as f:
+    f.write(f"{sys.argv[1]},{(end-start)*1000}\n")
