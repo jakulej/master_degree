f2cf883428ecdaa35eecd6bed99fedf855e1b6c5
jakulej <j.kulej01@gmail.com>
2025-06-04 19:31:21 +0200

    added time mesure

--- tests/server_websocket.py
+++ tests/server_websocket.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import asyncio
+import json
 from websockets.asyncio.server import serve
 
-async def echo(websocket):
+async def client_handler(websocket):
     async for message in websocket:
-        await websocket.send(message)
+        data = json.loads(message)
+        print(data)
+        await websocket.send("OK")
 
 async def main():
-    async with serve(echo, "localhost", 8765) as server:
+    async with serve(client_handler, "localhost", 8765) as server:
         await server.serve_forever()
 
 asyncio.run(main())
