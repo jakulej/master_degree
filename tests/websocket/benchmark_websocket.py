2e95639645596d14544352a7f1033304218b9245
jakulej <j.kulej01@gmail.com>
2025-06-05 16:42:04 +0200

    websocket test done

--- tests/websocket/benchmark_websocket.py
+++ tests/websocket/benchmark_websocket.py
@@ -12,5 +12,15 @@ def n_times(request_number):
     result = subprocess.run(["python3", "tests/websocket/n_requests.py", str(request_number)], capture_output=True, text=True)
     print(result)
 
+def file(request_number):
+    output_file = "output/websocket_big.csv"
+    with open(output_file, "w") as f:
+        f.write("request_number,time_ms\n")
+    for n in range(request_number):
+        result = subprocess.run(["python3", "tests/websocket/file_request.py", str(request_number)], capture_output=True, text=True)
+        print(result)
+
+
 #single(100)
-n_times(22)
+#n_times(22)
+file(20)
