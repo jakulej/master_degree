484fceed65958957511b0b5ddd1b240ba9beaf2a
jakulej <j.kulej01@gmail.com>
2025-06-05 12:37:23 +0200

    changed lib for http

--- tests/http/single_request_small.py
+++ tests/http/single_request_small.py
@@ -1,18 +1,19 @@
 import httpx
 import time
 import json
-import sys 
+import sys
+import requests
 
 OUTPUT_FILE = "output/http_simple_small_independent.csv"
-URL = 'http://127.0.0.1:5000'
+URL = 'http://127.0.0.1:8000'
 
 with open('data/small_data.json') as file:
     small_data = json.load(file)
 
 start = time.perf_counter()
-response = httpx.post(URL,json=small_data)
+response = requests.post(URL,json=small_data)
 end = time.perf_counter()
-
+print(response)
 
 with open(OUTPUT_FILE, "a") as f:
     f.write(f"{sys.argv[1]},{(end-start)*1000}\n")
